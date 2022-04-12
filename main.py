eb754ee890b84a1710847903d18f8bd1bacaac0b
Bizzoibeck <Konrad.Beck.freiburg@gmail.com>
2022-04-12 14:19:25 +0200

    ads

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import numpy as np
 
-points = [[0, 0], [0.5, 0], [1, 0], [0, 0.5], [1, 0.5], [0, 1], [0.5, 1], [1, 1]]
-def randomWalk(p, q0, N):
-    np.random.seed(1246127846)
-    q=[]
-    q.append(q0)
-    print(q)
-
-    for n in range(0, N):
-
-        i = np.random.randint(0, 8)
-        q.append(np.divide(np.add(q[n],p[i]),3))
-        if n%(N/100)==0:
-            print(n/(N/100))
-
-    return q
+points = [[0, 0], [0.5, 0], [1, 0], [0, 0.5], [1, 0.5], [0, 1], [0.5, 1], [1, 1]]#Initialisierung der Punkte p_0  bis p_7
 
 
-print(randomWalk(points,points[0],100))
+def randomWalk(p, q0, N):                          # Implementierung des randomwalks
+    q = []                                         # Erstellen einer leeren Liste
+    q.append(q0)                                   # q_0 der Liste hinzufügen
 
+
+    for n in range(0, N):                          # Schleife von 0 bis N
+        i = np.random.randint(0, 8)                # i wird eine zufällige Ganzzahl zugewiesen
+        q.append(np.divide(np.add(q[n], p[i]), 3)) # Der Liste wird das Element q_n+1 hinzugefügt
+
+    return q                                       # Die funktion gibt q_0 bis q_N als Liste zurück
+
+
+def getN(q, e):                                    # Funktion zum zählen der nicht leeren Raster der Punkte q mit Gitterkonstante e
+    raster = np.zeros((int(0.5 / e), int(0.5 / e)))# Das Intervall [0;0]x[0.5;0.5] wird in e*e Quadrate eingeteilt
+
+    for i in q:                                    # Für alle q
+        x = int(i[0] / e)                          # Werden die Gitterkoordinaten x und y bestimmt
+        y = int(i[1] / e)
+        raster[x][y] = 1                           # Das Quadrat in welchem sich der Punkt befindet wird auf 1 gesetzt
+
+    return np.sum(raster)                          #N(e) entspricht dann der Summe der Quadratwerte
+
+
+
+def getFractalDim(q, e):                            # Implementierung der Formel (1) mit e_i= 1/2^i
+    return np.log(getN(q, e) / getN(q, e / 2)) / (np.log(1 / 2))
+
+
+
+print (getFractalDim(randomWalk(points,points[0],1000),e=1/128))
