be1bd7ea6215382e682ed517963ef7817eab585b
max <m.reicherd@posteo.de>
2022-05-01 15:54:47 +0200

    .

--- Übungsblatt2/CP_Blatt2_Beck_Reicherd.py
+++ Übungsblatt2/CP_Blatt2_Beck_Reicherd.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 a = pd.read_csv("abweichungdat.sec",sep=" ",encoding='ISO-8859-1')
 y1=a._get_column_array(1)
 x1=a._get_column_array(0)
-y=np.divide(y1,x1)
-plt.plot(x1,y)
-plt.show()
-print()
+y=np.log2(y1)
+
+for i in range(0,15):
+    y = np.divide(np.power(x1, i), math.factorial(i))
+    plt.plot(x1,y)
+    plt.show()
