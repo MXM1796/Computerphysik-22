480f5dcefb8688bab65073bb3960f12ab728f751
max <m.reicherd@posteo.de>
2022-05-01 16:39:56 +0200

    .

--- Übungsblatt2/CP_Blatt2_Beck_Reicherd.py
+++ Übungsblatt2/CP_Blatt2_Beck_Reicherd.py
@@ -7,7 +7,22 @@ y1=a._get_column_array(1)
 x1=a._get_column_array(0)
 y=np.log2(y1)
 
-for i in range(0,15):
+xn = []
+yn = []
+for i in range(1,2):
     y = np.divide(np.power(x1, i), math.factorial(i))
+    yn.append(y)
+    xn.append(x1)
+    print(xn,yn)
     plt.plot(x1,y)
     plt.show()
+
+
+# Teil 2
+
+ZweiPi = 2*np.pi
+print(ZweiPi)
+x_1 = 0.1
+testfunktion = np.cos(ZweiPi*x_1)
+
+# ZweiPunktFormel
