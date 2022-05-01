43007bb7b1ff2997d364940a61d51762e46dd9fe
max <m.reicherd@posteo.de>
2022-05-01 19:07:44 +0200

    .

--- Übungsblatt2/CP_Blatt2_Beck_Reicherd.py
+++ Übungsblatt2/CP_Blatt2_Beck_Reicherd.py
@@ -2,27 +2,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
-a = pd.read_csv("abweichungdat.sec",sep=" ",encoding='ISO-8859-1')
-y1=a._get_column_array(1)
-x1=a._get_column_array(0)
-y=np.log2(y1)
 
-xn = []
-yn = []
-for i in range(1,2):
-    y = np.divide(np.power(x1, i), math.factorial(i))
-    yn.append(y)
-    xn.append(x1)
-    print(xn,yn)
-    plt.plot(x1,y)
-    plt.show()
+a = pd.read_csv("abweichungdat.sec", sep=" ", encoding='ISO-8859-1')
+y1 = a._get_column_array(1)
+x1 = a._get_column_array(0)
+# y=np.log2(y1)
 
-
+# plt.plot(x1**0.3333, y1)
+# plt.show()
 # Teil 2
 
-ZweiPi = 2*np.pi
-print(ZweiPi)
-x_1 = 0.1
-testfunktion = np.cos(ZweiPi*x_1)
+ZweiPi = 2 * np.pi
+w_arr = np.arange(1, 21)
+h = np.linspace(1, 2, 5)
+print(h)
 
-# ZweiPunktFormel
+
+# Zwei Punkt Formel
+def d_y_approx_2(w, h):
+    d_y_a_3 = np.divide(np.cos(ZweiPi * (w + h) * (0.1 / (w + h))) - np.cos(ZweiPi * w * (0.1 / w)), ((w + h) - w))
+    return d_y_a
+
+
+def d_y_exakt(w):
+    d_y_e = - (ZweiPi * w) * np.sin(ZweiPi * w * (0.1 / w))
+    return d_y_e
+
+# drei Punkt Formel
+def d_y_approx_3(w, h):
+    d_y_a_3 = np.divide(np.cos(ZweiPi * (w + h) * (0.1 / (w + h))) - np.cos(ZweiPi * (w - h) * (0.1 / (w - h))), 2*((w - h)- w))
+    return d_y_a_3
+
+
+def d_y_exakt(w):
+    d_y_e = - (ZweiPi * w) * np.sin(ZweiPi * w * (0.1 / w))
+    return d_y_e
